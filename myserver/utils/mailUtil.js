91113363d2b182f6a7d13988dd6b9f5765384b5e
DESKTOP-UEUSO3R\mrbin <hhhien2903@gmail.com>
2023-12-17 18:10:27 +0700

    order confirmation_email

---
+++ myserver/utils/mailUtil.js
@@ -0,0 +1,19 @@
+const nodemailer = require('nodemailer');
+const hbs = require('hbs');
+const path = require('path');
+
+const gmailTransporter = nodemailer.createTransport({
+  pool: true,
+  host: process.env.GMAIL_SMTP_HOST,
+  port: process.env.GMAIL_SMTP_PORT,
+  secure: true,
+  auth: {
+    user: process.env.GMAIL_SMTP_USER,
+    pass: process.env.GMAIL_SMTP_PASS,
+  },
+  tls: { rejectUnauthorized: false },
+});
+
+module.exports = {
+  gmailTransporter,
+};
